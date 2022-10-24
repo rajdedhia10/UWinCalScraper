b004d7a4ed7cdd57a51e331bb16801b277d60e98
Raj Dedhia <rajdedhia10@gmail.com>
2022-10-23 23:24:18 -0400

    It works yay

--- Scraper.py
+++ Scraper.py
@@ -1,8 +1,8 @@
 from __future__ import print_function
 import requests
 from bs4 import BeautifulSoup
+from datetime import *
 
-import datetime
 import os.path
 
 from google.auth.transport.requests import Request
@@ -14,8 +14,43 @@ from googleapiclient.errors import HttpError
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
+# Convert time from 12 hrs to RFC3339 standard
+def time_convert(t):
+    t = datetime.strptime(t, '%I:%M%p')
+    return(t.time())
 
-def scraper():
+# To insert the events to Calendar via API v3
+def inserter(dict,creds):
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+
+        # Template to Insert an event
+        event = {
+        'summary': 'CS Dept Event - '+ dict.get('event_date'),
+        'description': dict.get('event_title'),
+        'start': {
+            'dateTime': dict.get('event_date')+'T'+dict.get('start_time')+'-04:00',
+            'timeZone': 'Canada/Eastern',
+        },
+        'end': {
+            'dateTime': dict.get('event_date')+'T'+dict.get('end_time')+'-04:00',
+            'timeZone': 'Canada/Eastern',
+        },
+        'reminders': {
+            'useDefault': True,
+        },
+        }
+
+        # Call the Calendar Insert API
+        event = service.events().insert(calendarId='9e9380475a6f0032566930b75eea2fa6d0cee286c1dcf4122ce3895077c0216a@group.calendar.google.com', body=event).execute()
+        print('Event created: %s' % (event.get('htmlLink')))
+
+    except HttpError as error:
+        print('An error occurred: %s' % error)
+
+
+# To scrape the website and get the events
+def scraper(creds):
     URL = "https://www.uwindsor.ca/science/computerscience/event-calendar/month"
     page = requests.get(URL)
 
@@ -40,31 +75,40 @@ def scraper():
     #     event_time = cal2[i].find(class_="event-date").text.strip()
     #     print(event_time)
 
+    dict = {
+        'event_title':'',
+        'event_date':'',
+        'event_link':'',
+        'event_time':'',
+        'start_time':'',
+        'end_time':''
+    }
+
     for i in range (len(cal3)):
-        print("\n\n")
-        event_title = cal3[i].find(class_="event-title").text.strip()
-        print(event_title)
-        event_date = cal3[i]["data-date"]
-        print(event_date)
-        event_link = "www.uwindsor.ca"+ cal3[i].find('a', href=True)['href']
-        print(event_link)
+        dict.update({'event_title': cal3[i].find(class_="event-title").text.strip()})
+        # print(event_title)
+        dict.update({'event_date': cal3[i]["data-date"]})
+        # print(event_date)
+        dict.update({'event_link':"www.uwindsor.ca"+ cal3[i].find('a', href=True)['href']})
+        # print(event_link)
+        dict.update({'event_time': cal3[i].find(class_="event-date").text.strip()})
         event_time = cal3[i].find(class_="event-date").text.strip()
-        print(event_time)
+        # print(event_time)
 
-def inserter(creds):
-    try:
-        service = build('calendar', 'v3', credentials=creds)
+        start_time = event_time[:event_time.find(' ')]
+        end_time = event_time[event_time.rfind(' ')+1:]
 
-        # Call the Calendar API
-        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-        print('Getting the upcoming 10 events')
-        events_result = service.events().list(calendarId='primary', timeMin=now,
-                                              maxResults=10, singleEvents=True,
-                                              orderBy='startTime').execute()
-        events = events_result.get('items', [])
+        dict.update({'start_time': str(time_convert(start_time))})
+        dict.update({'end_time': str(time_convert(end_time))})
 
-    except HttpError as error:
-        print('An error occurred: %s' % error)
+        
+        # print(time_convert(start_time))
+        # print(time_convert(end_time))
+        # print(time_convert(start_time))
+        # print(time_convert(end_time))
+        # print(dict)
+        inserter(dict,creds)
+
 
 def main():
     creds = None
@@ -87,10 +131,11 @@ def main():
 
     try:
         page_token = None
-        service = build('calendar', 'v3', credentials=creds)
-        calendar_list = service.calendarList().list(pageToken=page_token).execute()
-        for calendar_list_entry in calendar_list['items']:
-            print(calendar_list_entry['summary'])
+        # service = build('calendar', 'v3', credentials=creds)
+        # calendar_list = service.calendarList().list(pageToken=page_token).execute()
+        # for calendar_list_entry in calendar_list['items']:
+        #     print(calendar_list_entry['summary'])
+        scraper(creds)
 
 
     except HttpError as error:
--- test.py
+++ test.py
@@ -1,5 +1,7 @@
+from re import T
 import requests
 from bs4 import BeautifulSoup
+from datetime import *
 
 URL = "https://www.uwindsor.ca/science/computerscience/event-calendar/month"
 page = requests.get(URL)
@@ -24,16 +26,60 @@ cal3 = soup.find_all(class_="single-day future")
 #     print(event_link)
 #     event_time = cal2[i].find(class_="event-date").text.strip()
 #     print(event_time)
+def time_convert(t):
+    # Set the time in 12-hour format
+    # current_time = '2:00PM'
+    # print("Time = ",t)
+
+    # Convert 12 hour time to 24 hour format
+    t = datetime.strptime(t, '%I:%M%p')
+    return(t.time())
+
+
+dict = {
+        'event_title':'',
+        'event_date':'',
+        'event_link':'',
+        'event_time':'',
+        'start_time':'',
+        'end_time':''
+    }
 
 for i in range (len(cal3)):
+    # print("\n\n")
+    # event_title = cal3[i].find(class_="event-title").text.strip()
+    # print(event_title)
+    # event_date = cal3[i]["data-date"]
+    # print(event_date)
+    # event_link = "www.uwindsor.ca"+ cal3[i].find('a', href=True)['href']
+    # print(event_link)
+    # event_time = cal3[i].find(class_="event-date").text.strip()
+    # print(event_time)
+
     print("\n\n")
-    event_title = cal3[i].find(class_="event-title").text.strip()
-    print(event_title)
-    event_date = cal3[i]["data-date"]
-    print(event_date)
-    event_link = "www.uwindsor.ca"+ cal3[i].find('a', href=True)['href']
-    print(event_link)
+    dict.update({'event_title': cal3[i].find(class_="event-title").text.strip()})
+    # print(event_title)
+    dict.update({'event_date': cal3[i]["data-date"]})
+    # print(event_date)
+    dict.update({'event_link':"www.uwindsor.ca"+ cal3[i].find('a', href=True)['href']})
+    # print(event_link)
+    dict.update({'event_time': cal3[i].find(class_="event-date").text.strip()})
     event_time = cal3[i].find(class_="event-date").text.strip()
-    print(event_time)
+    # print(event_time)
 
+    start_time = event_time[:event_time.find(' ')]
+    end_time = event_time[event_time.rfind(' ')+1:]
 
+    dict.update({'start_time': str(time_convert(start_time))})
+    dict.update({'end_time': str(time_convert(end_time))})
+    # print(time_convert(start_time))
+    # print(time_convert(end_time))
+    print(dict)
+
+    print(dict.get('event_date')+'T'+dict.get('start_time')+'-04:00')
+    print(dict.get('event_date')+'T'+dict.get('end_time')+'-04:00')
+
+    # start_time = event_time[:event_time.find(' ')]
+    # end_time = event_time[event_time.rfind(' ')+1:]
+    # print(time_convert(start_time))
+    # print(time_convert(end_time))
